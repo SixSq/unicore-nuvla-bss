ba5d4eb6814fd87e7b9b8886f98896bb5031995c
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2017-10-29 23:29:13 +0100

    full workflow with unittest

--- lib/BSS.py
+++ lib/BSS.py
@@ -9,15 +9,33 @@ import Utils
 from BSSCommon import BSSBase
 
 from slipstream.api.api import Api
-from boto.s3.connection import S3Connection
-from boto.s3.key import Key
+from boto.s3.connection import S3Connection, Key, Bucket
 
 CLOUD = 'exoscale'
+CLOUD_CONN_NAME = 'exoscale-ch-gva'
 CLOUD_CRED_NAME_PREF = 'hbp_mooc'
 BUCKET_NAME_PREF = CLOUD_CRED_NAME_PREF
+USERSPACE_RTP = 'userspace-endpoint'
+
+COMP_NAME = 'compute'
+
+JOB_STATE_MAP = {'initializing': 'QUEUED',
+                 'provisioning': 'QUEUED',
+                 'executing': 'RUNNING',
+                 'sendingreports': 'RUNNING',
+                 'ready': 'COMPLETED',
+                 'finalizing': 'COMPLETED',
+                 'done': 'COMPLETED',
+                 'cancelled': 'COMPLETED'
+                 # '???': 'SUSPENDED',
+                 }
 
 
 class BSS(BSSBase):
+    def _nuvla_creds(self, message):
+        key_secret = Utils.extract_parameter(message, "IDENTITY")
+        if key_secret:
+            return key_secret.split(':')
 
     @staticmethod
     def nuvla(message):
@@ -49,15 +67,15 @@ class BSS(BSSBase):
         return "nuvla"
 
     def _get_s3_creds(self, nuvla):
-        creds = list(nuvla.get_cloud_credentials(
-            cimi_filter="type='cloud-cred-%s'" % CLOUD))
+        cfilter = "type='cloud-cred-%s' and name^='%s'" % (CLOUD,
+                                                           CLOUD_CRED_NAME_PREF)
+        creds = list(nuvla.get_cloud_credentials(cimi_filter=cfilter))
         if len(creds) < 1:
-            raise Exception('Failed to find cloud creds for %s.' % CLOUD)
-        for c in creds:
-            if c.name.startswith(CLOUD_CRED_NAME_PREF):
-                return c.key, c.secret
-        raise Exception('Failed to find %s cloud credentials with '
-                        'the name starting with %s.' % (CLOUD, CLOUD_CRED_NAME_PREF))
+            raise Exception('Failed to find %s cloud credentials with '
+                            'the name starting with %s.' % (
+                                CLOUD, CLOUD_CRED_NAME_PREF))
+        c = creds[0]
+        return c.key, c.secret
 
     def _get_s3_connection(self, nuvla):
         key, secret = self._get_s3_creds(nuvla)
@@ -66,11 +84,11 @@ class BSS(BSSBase):
             aws_secret_access_key=secret,
             host='sos.exo.io')
 
-    def _stage_in_to_s3(self, nuvla, message):
-        """Returns S3 Key of the directory where the files where staged in.
+    def _put_files_to_s3(self, nuvla, message):
+        """Returns S3 Key of the directory where the files were staged in.
         :param nuvla: slipstream.api.api.Api
         :param message: TSI message
-        :return: boto.s3.key.Key
+        :return: boto.s3.key.Key - directory where files were staged.
         """
         s3 = self._get_s3_connection(nuvla)
         bucket_name = '%s_%s' % (
@@ -78,12 +96,59 @@ class BSS(BSSBase):
             hashlib.md5(nuvla.username.encode()).hexdigest())
         bucket = s3.create_bucket(bucket_name, policy='private')
         bucket_stage_dir = str(int(time.time() * 1000))
+        in_dir = '%s/input/' % bucket_stage_dir
+        out_dir = '%s/output/' % bucket_stage_dir
+        for d in [in_dir, out_dir]:
+            k = bucket.new_key(d)
+            k.set_contents_from_string('')
         files = self._get_stagein_files(message)
         for f in files:
             fn = os.path.basename(f)
-            key = Key(bucket, '%s/%s' % (bucket_stage_dir, fn))
+            key = bucket.new_key('%s%s' % (in_dir, fn))
             key.set_contents_from_filename(f)
-        return Key(bucket, bucket_stage_dir)
+        return Key(bucket, bucket_stage_dir + '/')
+
+    def _download_files_from_s3(self, message, nuvla):
+        bucket, dir_name = self._get_s3_scratch_dir(message, nuvla)
+        local_path = Utils.extract_parameter(message, "USPACE")
+        if not local_path:
+            raise Exception('Failed to get local path to files as USPACE.')
+        for k in bucket.list(prefix=('%s/output/' % dir_name)):
+            if k.name.endswith('/'):
+                continue
+            fn = '%s/%s' % (local_path.rstrip('/'), os.path.basename(k.name))
+            k.get_contents_to_filename(fn)
+
+    def _get_s3_scratch_dir(self, message, nuvla):
+        """Returns bucket and scratch directory name.
+        :param message:
+        :param nuvla:
+        :return: (Bucket, str)
+        """
+        duid = Utils.extract_parameter(message, "BSSID")
+        if not duid:
+            raise Exception('Failed to get deployment uuid as BSSID.')
+        s3_path = self._get_scratch_path(nuvla, duid)
+        if not s3_path:
+            raise Exception('Failed to get S3 path.')
+        bucket_name, dir_name = s3_path.split('/')[0:2]
+        s3 = self._get_s3_connection(nuvla)
+        bucket = Bucket(s3, bucket_name)
+        return bucket, dir_name
+
+    def _delete_s3_scratch_space(self, message, nuvla):
+        """Deletes scratch space on S3.
+        """
+        bucket, dir_name = self._get_s3_scratch_dir(message, nuvla)
+        for k in bucket.list(dir_name):
+            k.delete()
+
+    def _apicred_params(self, message):
+        return dict(zip(["api-key", "api-secret"], self._nuvla_creds(message)))
+
+    def _get_scratch_path(self, nuvla, duid):
+        param = '%s.1:%s' % (COMP_NAME, USERSPACE_RTP)
+        return nuvla.get_deployment_parameter(duid, param, ignore_abort=True)
 
     def submit(self, message, connector, config, LOG):
         try:
@@ -95,10 +160,17 @@ class BSS(BSSBase):
         if not app:
             connector.failed('No application URI provided.')
             return
-        self._stage_in_to_s3(nuvla, message)
         try:
-            dpl_id = nuvla.deploy(app)
-            connector.ok(dpl_id)
+            s3_stage_path = self._put_files_to_s3(nuvla, message)
+            compute_params = {
+                USERSPACE_RTP: "%s/%s" % (s3_stage_path.bucket.name,
+                                          s3_stage_path.name)}
+            # FIXME: this will be removed when API key authn is available on VM.
+            compute_params.update(self._apicred_params(message))
+            params = {"compute": compute_params}
+            dpl_id = nuvla.deploy(app, cloud={"compute": CLOUD_CONN_NAME},
+                                  parameters=params, keep_running='never')
+            connector.ok(str(dpl_id))
             return
         except Exception as ex:
             connector.failed(str(ex))
@@ -106,21 +178,31 @@ class BSS(BSSBase):
     def get_status_listing(self, message, connector, config, LOG):
         result = ['QSTAT']
         nuvla = self.nuvla(message)
-        for dpl in nuvla.list_deployments():
-            result.append('%s %s' % (dpl.uuid, dpl.status))
+        for dpl in nuvla.list_deployments(cloud=CLOUD_CONN_NAME):
+            result.append('%s %s' % (dpl.id,
+                                     self.convert_status(dpl.status)))
         connector.write_message('\n'.join(result) + '\n')
 
     def get_job_details(self, message, connector, config, LOG):
-        dpl_uuid = Utils.extract_parameter(message, "BSSID")
+        duid = Utils.extract_parameter(message, "BSSID")
+        if not duid:
+            connector.failed('TSI_BSSID was not provided.')
+            return
         nuvla = self.nuvla(message)
-        state = nuvla.get_deployment_parameter(dpl_uuid, 'ss:state',
-                                         ignore_abort=True)
+        state = self.convert_status(
+            nuvla.get_deployment_parameter(duid, 'ss:state',
+                                           ignore_abort=True))
+        if state == 'COMPLETED':
+            self._download_files_from_s3(message, nuvla)
+            self._delete_s3_scratch_space(message, nuvla)
         connector.ok(state)
 
     def abort_job(self, message, connector, config, LOG):
-        bssid = Utils.extract_parameter(message, "BSSID")
-        nuvla = self.nuvla()
-        nuvla.terminate(bssid)
+        duid = Utils.extract_parameter(message, "BSSID")
+        nuvla = self.nuvla(message)
+        nuvla.terminate(duid)
+        self._download_files_from_s3(message, nuvla)
+        self._delete_s3_scratch_space(message, nuvla)
         connector.ok()
 
     cancel_job = abort_job
@@ -138,12 +220,7 @@ class BSS(BSSBase):
 
     def convert_status(self, bss_state):
         """ converts BSS status to UNICORE status """
-        ustate = "UNKNOWN"
-        if bss_state.lower() in ["", "running"]:
-            ustate = "RUNNING"
-        elif bss_state == "T":
-            ustate = "SUSPENDED"
-        return ustate
+        return JOB_STATE_MAP.get(bss_state.lower(), 'UNKNOWN')
+
     def create_submit_script(self, message, config, LOG):
         return []
-
--- tests/test_BSS_Nuvla_live.py
+++ tests/test_BSS_Nuvla_live.py
@@ -1,11 +1,19 @@
 import os
+import re
+import time
 import shutil
 import tempfile
 import pytest
-from BSS import BSS, BUCKET_NAME_PREF
+from BSS import BSS, BUCKET_NAME_PREF, JOB_STATE_MAP
+from MockConnector import MockConnector
 from boto.s3.key import Key
 
+uuid_re = re.compile(
+    '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}')
+
 pytestmark = pytest.mark.live
+
+NUVLA_APP = 'konstan/hbp_mooc/compute-cluster'
 
 nuvla_apikey = ''
 nuvla_apisecret = ''
@@ -29,6 +37,15 @@ def test_get_cloud_creds():
     assert cloud_secret == secret
 
 
+def cleanup_s3_staging(s3_path):
+    try:
+        if s3_path:
+            for e in s3_path.bucket.list(prefix=s3_path.name):
+                e.delete()
+    except:
+        pass
+
+
 @need_nuvla_creds
 def test_stage_in_to_s3():
     path = tempfile.mkdtemp()
@@ -45,14 +62,80 @@ def test_stage_in_to_s3():
 """ % (nuvla_apikey, nuvla_apisecret, path)
         nuvla = BSS.nuvla(message)
         bss = BSS()
-        s3_path = bss._stage_in_to_s3(nuvla, message)
+        s3_path = bss._put_files_to_s3(nuvla, message)
         assert isinstance(s3_path, Key)
         assert s3_path.bucket.name.startswith(BUCKET_NAME_PREF)
     finally:
         shutil.rmtree(path)
-        try:
-            if s3_path:
-                for e in s3_path.bucket.list(prefix=s3_path.name):
-                    e.delete()
-        except:
-            pass
+        cleanup_s3_staging(s3_path)
+
+
+def test_workflow():
+    path = tempfile.mkdtemp()
+    files = set()
+    for i in range(2):
+        f = os.path.join(path, '%s.txt' % i)
+        open(f, 'a').close()
+        files.update([f])
+    s3_path = None
+    message = """#!/bin/bash
+#TSI_IDENTITY %s:%s
+#TSI_USPACE %s
+#TSI_SCRIPT
+UC_EXECUTABLE=%s
+""" % (nuvla_apikey, nuvla_apisecret, path, NUVLA_APP)
+    print 'initial message:', message
+    try:
+
+        bss = BSS()
+
+        # submit deployment.
+        connector = MockConnector(None, None, None, None, None)
+        bss.submit(message, connector, None, None)
+
+        # assert deployment UUID was set on the TSI connector.
+        duid = ''
+        for line in connector.control_out.getvalue().split('\n'):
+            if uuid_re.match(line):
+                duid = line
+        assert len(duid) == 36
+
+        # Insert #TSI_BSSID to the message.
+        message_ = filter(lambda x: len(x) > 0, message.split('\n'))
+        message_.insert(1, "#TSI_BSSID %s" % duid)
+        message = '\n'.join(message_)
+        print 'updated message:', message
+
+        connector = MockConnector(None, None, None, None, None)
+        bss.get_status_listing(message, connector, None, None)
+        assert duid in connector.control_out.getvalue()
+
+        # set s3_path for the later cleanup.
+        nuvla = BSS.nuvla(message)
+        scratch_path = bss._get_scratch_path(nuvla, duid)
+        bucket_name, dir_name = scratch_path.split('/')[0:2]
+        s3 = bss._get_s3_connection(nuvla)
+        bucket = s3.create_bucket(bucket_name)
+        s3_path = Key(bucket, dir_name)
+        print "S3 path", s3_path.name
+
+        # wait for Done state
+        while True:
+            connector = MockConnector(None, None, None, None, None)
+            bss.get_job_details(message, connector, None, None)
+            state = connector.control_out.getvalue().split('\n')[1]
+            assert state in JOB_STATE_MAP.values()
+            print 'Deployment %s state %s' % (duid, state)
+            if state == 'COMPLETED':
+                print 'Job completed.'
+                break
+            print 'Sleeping 5 sec.'
+            time.sleep(5)
+
+        assert os.path.exists('%s/%s' % (path, 'result.txt'))
+        assert [] == list(bucket.list(dir_name))
+
+    finally:
+        pass
+        shutil.rmtree(path)
+        cleanup_s3_staging(s3_path)
